99cad00bbfcacc0f97c054631f5bf4f9cdda8666
Re-Panza <111627325+Re-Panza@users.noreply.github.com>
2026-02-11 23:16:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // --- CONFIGURAZIONE ---
 // Ricorda di inserire qui il link del tuo ultimo deployment
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwJCmKFZboR7-h1zzwTidy7uygfjt-2cAOn1p4bArhl/dev";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyZ6OSR-4JvqTqls5kro0fIfpxrcOjzOd5zttQV36e8BtDSp5Gr7h8VWGLP-FnWpro9lA/exec";
 
 // --- VARIABILI GLOBALI ---
 let currentGame = null;
